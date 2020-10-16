a9c9d0a1a86888d9ead4b99e5c04875d0e34cda8
brandonleetheasian <bjlee@uvm.edu>
2020-10-16 19:00:20 -0400

    restuarant class finished

--- customer.py
+++ customer.py
@@ -1,10 +1,9 @@
 class Customer:
-    def __init__(self, name, phone_number, order, precedence):
+    def __init__(self, name, phone_number, order):
         self.name = name
         self.phone_number = phone_number
         self.served = False
         self.order = order
-        self. precedence = precedence
 
 # Getters
     def get_name(self):
@@ -19,9 +18,6 @@ class Customer:
     def get_order(self):
         return self.order
 
-    def get_precedence(self):
-        return self.precedence
-
     # add_t0_order --> accepts dish objects
     # remove from order --> accepts dish objects
     # served
@@ -34,16 +30,12 @@ class Customer:
 
     def served(self):
         self.served = True
-        self.precedence = -1
-
-    def move_up_precedence(self):
-        self.precedence = self.precedence - 1
 
     def to_string(self):
         str_served = "Waiting to be Served"
         if self.served:
             "Already Served"
-        s = self.name + ', ' + 'precedence: ' + self.precedence + ', ' + str_served
+        s = self.name + ', ' + str_served
         return s
 
     def __eq__(self, other):
--- restaurant.py
+++ restaurant.py
@@ -1,33 +1,38 @@
-from queue import PriorityQueue
+from customer import Customer
+
 
 class Restaurant:
     def __init__(self, menu):
         self.menu = menu
-        self.order_line_up = PriorityQueue
         self.money_made = 0.0
-        self.customers = []
+        self.customer_line_up = []
+        self.completed_orders = []
 
     def get_menu(self):
         return self.menu
 
-    def get_customers(self):
-        return self.customers
+    def get_customer_line_up(self):
+        return self.customer_line_up
 
     def get_money_made(self):
         return self.money_made
 
-    def get_order_line_up(self):
-        return self.order_line_up
-
-    #add_customer(Customer)
-
     def add_customer(self, customer):
-        self.customers.append(customer)
-        self.order_line_up.put(customer.get_precedence, customer.get_order())
+        self.customer_line_up.append(customer)
 
+    def find_precedence(self, customer):
+        precedence = self.customer_line_up.index(customer) + 1
+        return precedence
 
-    def find_precendence(self, customer):
-        return customer.get_precedence()
+    def take_order(self, name, number, order):
+        customer = Customer(name, number, order)
+        self.customer_line_up.append(customer)
 
+    def cancel_order(self, customer):
+        self.customer_line_up.remove(self.customer_line_up.index(customer))
 
-
+    def completed_recent_order(self):
+        customer = self.customer_line_up.pop(0)
+        order_finished = customer.get_order()
+        self.money_made += order_finished.calculate_cost
+        self.completed_orders.append(customer)
