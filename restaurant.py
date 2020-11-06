ee94e4c2cacc40b408e6d38764650192882cef1a
brandonleetheasian <bjlee@uvm.edu>
2020-11-05 19:46:25 -0500

    restuarant good

--- restaurant.py
+++ restaurant.py
@@ -21,18 +21,29 @@ class Restaurant:
         self.customer_line_up.append(customer)
 
     def find_precedence(self, customer):
-        precedence = self.customer_line_up.index(customer) + 1
-        return precedence
+        if customer not in self.customer_line_up:
+            return -1
+        else:
+            precedence = self.customer_line_up.index(customer) + 1
+            return precedence
 
     def take_order(self, name, number, order):
         customer = Customer(name, number, order)
         self.customer_line_up.append(customer)
 
     def cancel_order(self, customer):
-        self.customer_line_up.remove(self.customer_line_up.index(customer))
+        if customer not in self.customer_line_up:
+            return -1
+        else:
+            self.customer_line_up.remove(self.customer_line_up.index(customer))
+            return 0
 
     def completed_recent_order(self):
-        customer = self.customer_line_up.pop(0)
-        order_finished = customer.get_order()
-        self.money_made += order_finished.calculate_cost
-        self.completed_orders.append(customer)
+        if len(self.customer_line_up) == 0:
+            return -1
+        else:
+            customer = self.customer_line_up.pop(0)
+            order_finished = customer.get_order()
+            self.money_made += order_finished.calculate_cost
+            self.completed_orders.append(customer)
+            return 0
