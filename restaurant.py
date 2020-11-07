f1f8b8d9ca45f5194105769d8750b8eaf9052000
brandonleetheasian <bjlee@uvm.edu>
2020-11-07 15:52:46 -0500

    fixed add unwanted ingredient function

--- restaurant.py
+++ restaurant.py
@@ -152,21 +152,22 @@ class Restaurant:
     # the dish in the order, and then if the ingredient is in the dish. If all are true,
     # Do the operation and return 0. Else, return -1
 
-    def add_unwanted_ingredients(self, unwanted_ingredient, customer, dish_object):
+    def add_unwanted_ingredients(self, unwanted_ingredient, customer, dish):
         # see if the customer is in the line up
         if customer not in self.customer_line_up:
             return -1
         # if the customer is in the line up, get the order
         else:
-            temp_order = customer.get_order()
-            # get the dishes from the order
-            temp_order.order.get_checkout()
+            # find index of customer in lineup
+            index = self.customer_line_up.index(customer)
+            # get order
+            c_order = self.customer_line_up[index]
+            # get dishes from order
+            dish_list = c_order.get_checkout()
 
             # if the given dish object is in the list of dishes from the order
-            if dish_object in temp_order:
-                # remove unwanted ingredient
-                dish_object.dish.add_unwanted_ingredients(unwanted_ingredient)
-                return 0
+            if dish in dish_list:
+                return dish.add_unwanted_ingredients(unwanted_ingredient)
             # if not in list of dishes
             else:
                 return -1
